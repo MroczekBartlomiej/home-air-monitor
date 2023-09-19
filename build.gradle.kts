3099254a880f3772f13ce960d9d55c2f8705134d
Bartłomiej Mroczek <bartlomiejmroczek@gmail.com>
2023-09-19 21:32:34 +0200

    Config to build fat jar.

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,3 +39,9 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
+
+ktor {
+    fatJar {
+        archiveFileName.set("temperatureMonitor.jar")
+    }
+}
